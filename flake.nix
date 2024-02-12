197d6df8e4f3fac17a624d0eb79aaac5d679429f
Dennis Bogers <d.bogers@korsit.com>
2024-02-12 23:03:31 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,69 @@
+{
+  description = "Example Darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, home-manager, nix-darwin, nixpkgs }:
+  let
+    configuration = { pkgs, ... }: {
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages =
+        [ pkgs.vim
+        ];
+
+      # Auto upgrade nix package and the daemon service.
+      services.nix-daemon.enable = true;
+      # nix.package = pkgs.nix;
+
+      # Allow the installation of unfree packages
+      nixpkgs.config.allowUnfree = true;
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Create /etc/zshrc that loads the nix-darwin environment.
+      programs.zsh.enable = true;  # default shell on catalina
+      # programs.fish.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 4;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+
+      # Allow sudo using Touch ID
+      security.pam.enableSudoTouchIdAuth = true;
+
+      users.users."d.bogers".home = "/Users/d.bogers";
+    };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .
+    darwinConfigurations."svartalfheim" = nix-darwin.lib.darwinSystem {
+      modules = [ 
+	configuration
+        home-manager.darwinModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users."d.bogers" = import ./home.nix;
+        } 
+      ];
+    };
+
+    # Expose the package set, including overlays, for convenience.
+    darwinPackages = self.darwinConfigurations."svartalfheim".pkgs;
+  };
+}
---
+++ home.nix
@@ -0,0 +1,146 @@
+{ pkgs, lib, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "d.bogers";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "23.11"; # Please read the comment before changing.
+  
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = with pkgs;[
+    # Nix-specific packages
+    nil
+    # DevOps tools
+    argocd
+    awscli2
+    azure-cli
+    bruno
+    k9s
+    kubectl
+    kubernetes-helm
+    ssm-session-manager-plugin
+    terraform
+    popeye
+    # Development tools
+    firebase-tools
+    gitkraken
+    mariadb
+    nodejs-18_x
+    iterm2
+    jdk21
+    maven
+    # Container tools
+    colima
+    dive
+    docker-client
+    docker-buildx
+    # Kubernetes
+    minikube
+    skaffold
+    # Security
+    gobuster
+    john
+    nmap
+    sqlmap
+    # Communications
+    discord
+    slack
+    # Utility apps
+    keepassxc
+    obsidian
+    slides
+    # Random shell stuff
+    fd
+    ripgrep
+  ];
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # Enable direnv
+  programs.direnv = {
+    enable = true;
+    nix-direnv = {
+      enable = true;
+    };
+  };
+
+  programs.zsh = {
+    enable = true;
+    enableAutosuggestions = true;
+    enableCompletion = true;
+    dotDir = ".config/zsh";
+    syntaxHighlighting = {
+      enable = true;
+    };
+    initExtra = ''
+      if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
+        . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+      fi
+
+      if [ -d "/opt/homebrew/opt/ruby/bin" ]; then
+        export PATH=/opt/homebrew/opt/ruby/bin:$PATH
+        export PATH=`gem environment gemdir`/bin:$PATH
+      fi
+
+      eval "$(/opt/homebrew/bin/brew shellenv)"
+      export JAVA_HOME=${pkgs.jdk21.home}
+    '';
+    shellAliases = {
+      k = "kubectl";
+      tf = "terraform";
+      update = "home-manager switch";
+    };
+    plugins = [
+      {
+        name = "powerlevel10k";
+        src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+       }
+       {
+         name = "powerlevel10k-config";
+         src = lib.cleanSource ./p10k-config;
+         file = "p10k.zsh";
+       }
+    ];
+  };
+
+  # Configure my IDE, VSCode
+  programs.vscode = {
+    enable = true;
+    extensions = with pkgs.vscode-extensions; [
+      dbaeumer.vscode-eslint
+      eamodio.gitlens
+      gitlab.gitlab-workflow
+      hashicorp.terraform
+      jnoortheen.nix-ide
+      mkhl.direnv
+      ms-azuretools.vscode-docker
+      octref.vetur
+      redhat.vscode-yaml
+      rust-lang.rust-analyzer
+      yzhang.markdown-all-in-one
+      zhuangtongfa.material-theme
+    ];
+  };
+
+  programs.neovim = {
+    enable = true;
+    plugins = with pkgs.vimPlugins; [
+      LazyVim
+    ];
+  };
+
+  programs.gpg = {
+    enable = true;
+  };
+}
